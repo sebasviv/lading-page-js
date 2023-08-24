edf2e200c4b56e1d356728f455b0d8079e8eba3e
sebasviv <sebastianandresviveros@gmail.com>
2023-08-24 15:24:17 -0500

    base project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js", // Agrega [contenthash]
+    assetModuleFilename: 'src/assets/[name].[ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(html)$/,
+        use: ['html-loader']
+     }
+
+    ],
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      minify: false,
+    }),
+  ],
+};
