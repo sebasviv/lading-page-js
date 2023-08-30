ab4a6ae215e02068a0e3c13d310ad362d89f9042
sebasviv <sebastianandresviveros@gmail.com>
2023-08-30 16:55:29 -0500

    add header home responsive and menu hamburger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles/index.scss';
+
+
+const hamburguerMenu = document.getElementById('hamburger-menu');
+
+hamburguerMenu.addEventListener('click', () => { 
+    const menu = document.getElementById('hamburger-menu-content')
+    menu.style.display = 'flex';
+});
+
+const closeButtonMenu = document.getElementById('close-btn-menu');
+
+closeButtonMenu.addEventListener('click', () => { 
+    const menu = document.getElementById('hamburger-menu-content')
+    menu.classList.add('slide-out-right');
+    setTimeout(() => {
+        menu.style.display = 'none';
+        menu.classList.remove('slide-out-right');
+    }, 1000);
+});
+
